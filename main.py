95b1a2c2168cff9d62f5c2c3a1fe7c71da7f0e9c
1joooeel2 <90481601+1joooeel2@users.noreply.github.com>
2023-10-24 16:32:26 -0400

    # created new

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#IDK
+def encode(password):
+    new = ""
+    for i in password:
+        a = int(i) - 3
+        b = str(a)
+        new += b
+    return new
+
+def decode(password):
+    original = ""
+    for i in password:
+        a = int(i) - 3
+        b = str(a)
+        original += b
+    return original
+
+
