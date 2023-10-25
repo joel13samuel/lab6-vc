8c756d297f517ee30dd6e40c497d20e3e9c4a2f6
1joooeel2 <90481601+1joooeel2@users.noreply.github.com>
2023-10-25 11:00:11 -0400

    # added name and date.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #Joel Samuel
 #10/24
+
+def menu():
+    print("Menu ")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
 def encode(password):
     new = ""
     for i in password:
@@ -9,11 +18,28 @@ def encode(password):
     return new
 
 def decode(password):
-    original = ""
-    for i in password:
-        a = int(i) - 3
-        b = str(a)
-        original += b
-    return original
+    # Enter code here
+
+def main():
+    password = ""
+    encoded_password = ""
+
+    while True:
+        menu_option = int(input("Please enter an option: "))
+
+        if menu_option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+            print()
 
 
+        elif menu_option == 2:
+             # add decoder here
+             pass
+
+        elif menu_option == 3:
+                break
+
+if __name__ == '__main__':
+    main()
